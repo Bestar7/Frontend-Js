ca52102c86ade4ece3f4c15a519114cb97d1219e
Joachim IPL <joachim.bastin@student.vinci.be>
2021-12-13 14:46:04 +0100

    update Salon

--- src/components/Pages/Salon.js
+++ src/components/Pages/Salon.js
@@ -1,72 +1,34 @@
-import HomePage from "./HomePage";
 import { Redirect } from "../Router/Router";
 
-/**
- * Render the NewPage :
- * Just an example to demonstrate how to use the router to "redirect" to a new page
- */
+let idSalon;
 
- let idSalon;
- 
- let pageHtml=`
-  <div class= "row mx-0 text-center">
-    <div class="col-md-4 mt-2"> 
-      <h1 id="titleSalon" class= "display-2 mb-2 mb-md-5 text-center">Ze Question</h1>
-    </div>
-  </div>
-  `
-
-  let form=
-  `<div class="">
-  <div class="">
-      <h1 id="form" class="text-center">Form : </h1>
-      <form method="post" >
-        <div class="row justify-content-center">
-            <div class="">
-                <div class="row justify-content-center" style="padding-top:10px;">
-                    <input class="form-control" id="pseudo" type="text" name="name" placeholder="Pseudo">
-                </div>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-xs-3 col-md-5 col-sm-4" >
-                <div class="row justify-content-center" style="padding-top:10px;">
-                    <button class="btn btn-primary" type="submit" id="btnChatPage">Prêt</button>
-                </div>
-            </div>
-        </div>
-    </form>
-    
-  </div>
+const formHTML = 
+`<div id="form">
+  <form method="post">
+    <legend>Veuillez entrer votre pseudo pour cette partie</legend>
+    <input type="text" name="pseudo" placeholder="Pseudo">
+    <input type="submit" class="btn btn-primary" value="Prêt">
+    <button type="button" class="btn btn-primary" href="#" data-uri="/">Annuler</button>
+  </form>
 </div>`;
 
-let liste=``;
-    
- for (let index = 0; index < 4; index++) {
-  liste += `<li> index </li>`;
-}   
-
-
-
-
 function Salon() {
+  setBasicPage();
+  setLinks();
+}
+
+function setBasicPage() {
   document.title = 'Salon';
   // SET BASIC PAGE
   const pageDiv = document.querySelector("#page");
+  pageDiv.innerHTML = formHTML;
+}
 
-  pageDiv.innerHTML = pageHtml+form+ liste;
-
-  // CREATE BUTTON
-  const submit = document.createElement("button");
-  submit.innerHTML = "Go back to HomePage from salon";
-  submit.className = "btn btn-primary";
-
-  // COMPORTEMENT DES BUTTONS
-  submit.addEventListener("click", () => {
+function setLinks(){
+  const newSalon = document.querySelector("#form").getElementsByTagName("button");
+  newSalon[0].addEventListener("click", () => {
     Redirect("/");
   });
-  pageDiv.appendChild(submit);
-  
 }
 
 export default Salon;
