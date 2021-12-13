a85e2a5a8f9ac41bc88362f43158ca27a2d3c243
Joachim IPL <joachim.bastin@student.vinci.be>
2021-12-13 14:26:33 +0100

    update salonModo

--- src/components/Pages/SalonModo.js
+++ src/components/Pages/SalonModo.js
@@ -4,52 +4,37 @@ import { Redirect } from "../Router/Router";
  * TODO copier et modifier Salon.js pour que ça corresponde 
  */
 
- let idSalon;
- 
- let pageHtml=`
-  <div class= "row mx-0 text-center">
-    <div class="col-md-4 mt-2"> 
-      <h1 id="titleSalon" class= "display-2 mb-2 mb-md-5 text-center">Ze Question</h1>
-    </div>
-  </div>
-  `
+let idSalon;
 
-  let form=`<div class="row mx-0 text-center">
-  <div class="col-md-4"></div>
-  <div class="col-md-4 mt-2">
-      <h1 id="form" class="display-2 mb-2 mb-md-5 text-center">Form : </h1>
-      <form method="post" >
-        <div class="row justify-content-center">
-            <div class="col-xs-4">
-                <div class="row justify-content-center" style="padding-top:10px;">
-                    <input class="form-control" id="pseudo" type="text" name="name" placeholder="Pseudo">
-                </div>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-xs-3 col-md-5 col-sm-4" >
-                <div class="row justify-content-center" style="padding-top:10px;">
-                    <button class="btn btn-primary" type="submit" id="btnChatPage">Prêt</button>
-                </div>
-            </div>
-        </div>
+let form = 
+`<div>
+  <div>
+    <h1 id="form">Form : </h1>
+    <form method="post">
+      <input class="form-control" type="text" name="pseudo" placeholder="Pseudo">
+      <input type="submit" class="btn btn-primary" value="Prêt">
     </form>
-    
   </div>
 </div>`;
 
 let liste=``;
-    
  for (let index = 0; index < 4; index++) {
-  liste += `<li> index </li>`;
+  liste += `<li>index</li>`;
 }   
 
 
 
 
 function SalonModo() {
-  document.title = 'SalonAdmin';
-  
+  setBasicPage();
+}
+
+function setBasicPage() {
+  document.title = 'Salon Administrateur';
+  // SET BASIC PAGE
+  const pageDiv = document.querySelector("#page");
+  pageDiv.innerHTML = form;
+  console.log("set page done");
 }
 
 export default SalonModo;
