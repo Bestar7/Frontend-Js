609c5678984c9af2bdae08d7e5b3567cfca4bb93
Joachim BASTIN <joachim.bastin@student.vinci.be>
2021-12-13 12:21:12 +0100

    update homePage

--- src/components/Pages/homePage.js
+++ src/components/Pages/homePage.js
@@ -1,4 +1,5 @@
 
+import { Button } from "bootstrap";
 import { Redirect } from "../Router/Router";
 
 
@@ -8,77 +9,44 @@ import { Redirect } from "../Router/Router";
 */
 let test=`
 <div class="container d-flex justify-content-center">
-  <div id="home" class="flex-column flex-center">
+  <div id="home" class="flex-center">
+    
     <div id="connexion">
       <form id="form">
-        <label id="email"> </label>
         <input type="email" placeholder="email" name="email" type="text"/>
-        <label id="password"></label>
         <input type="password" placeholder="Mot de passe" name="password"/>
-        <a href="/login.html" class="btn btn-primary">Se connecter</a>
+        <a href="/login.html" class="btn btn-primary">Se connecter</a>    <!--TODO btn type submit-->
         <a href="/register.html" class="btn btn-primary">Creer une compte</a>
       </form>
     </div>
     <br>
 
-    <div id="joinSalon">
+    <div id="joinSalon" class="flex-center">
       <form>
         <input type = "text" placeholder="code salon" name="codeSalon"/>
-        <a href="/joinsalon.html" class="btn btn-primary"> Joindre salon</a>
-        </div>
-        </form>
+        <a href="/joinsalon.html" class="btn btn-primary"> Joindre salon</a>    <!--TODO btn type submit-->
+      </form>
     </div>
     <br>
 
-    <div id="joinSalon">
-      <button type="button" class="btn btn-primary" href="#" data-uri="/salon">Jouer</button>
+    <div id="newSalon" class="flex-center">
+      <button type="button" class="btn btn-primary" href="#" data-uri="/salon">Creer un salon</button>
     </div>
     <br>
 
-    <div id="newSalon">
-           
+    <div id="jouer" class="flex-center">
+      <button type="button" class="btn btn-primary" href="#" data-uri="/salonModo">Jouer</button>
     </div>
     <br>
 
-    
-
-    
-    
   </div>
 </div>`;
+
 function HomePage() {
     document.title = 'Ze Question';
     // SET BASIC PAGE
     const pageDiv = document.querySelector("#page");
     pageDiv.innerHTML = test;
-
-    // CREATE BUTTONS
-    //const btnCreateSalon = document.createElement("button");
-    const btnSalon = document.createElement("button");
-    const register = document.createElement("button");
-    //btnCreateSalon.innerHTML = "Creer un salon";
-    btnSalon.innerHTML = "Go back to Salon from Home";
-    register.innerHTML = "Créer un compte";
-    //btnCreateSalon.className = "btn btn-primary";
-    btnSalon.className = "btn btn-primary";
-    register.className = "btn btn-primary";
-
-    // COMPORTEMENT DES BUTTONS
-    /*
-    btnCreateSalon.addEventListener("click", () => {
-      Redirect("/salonModo");
-    });
-    document.createElement("newSalon").appendChild(btnCreateSalon);
-    */
-    btnSalon.addEventListener("click", () => {
-      Redirect("/salon");
-    });
-    pageDiv.appendChild(btnSalon);
-    register.addEventListener("click",() => {
-        Redirect("/register");
-    });
-    pageDiv.appendChild(register);
-
   }
   
   export default HomePage;
