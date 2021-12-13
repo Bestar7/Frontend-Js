1d1e6524b098f9c56c795e9a4b3b7726d767767b
Joachim IPL <joachim.bastin@student.vinci.be>
2021-12-13 14:46:12 +0100

    update salonModo

--- src/components/Pages/SalonModo.js
+++ src/components/Pages/SalonModo.js
@@ -1,40 +1,34 @@
 import { Redirect } from "../Router/Router";
 
-/**
- * TODO copier et modifier Salon.js pour que ça corresponde 
- */
-
 let idSalon;
 
-let form = 
-`<div>
-  <div>
-    <h1 id="form">Form : </h1>
-    <form method="post">
-      <input class="form-control" type="text" name="pseudo" placeholder="Pseudo">
-      <input type="submit" class="btn btn-primary" value="Prêt">
-    </form>
-  </div>
+const formHTML = 
+`<div id="form">
+  <form method="post">
+    <legend>Veuillez entrer votre pseudo pour cette partie</legend>
+    <input type="text" name="pseudo" placeholder="Pseudo">
+    <input type="submit" class="btn btn-primary" value="Prêt">
+    <button type="button" class="btn btn-primary" href="#" data-uri="/">Annuler</button>
+  </form>
 </div>`;
-
-let liste=``;
- for (let index = 0; index < 4; index++) {
-  liste += `<li>index</li>`;
-}   
-
-
-
 
 function SalonModo() {
   setBasicPage();
+  setLinks();
 }
 
 function setBasicPage() {
   document.title = 'Salon Administrateur';
   // SET BASIC PAGE
   const pageDiv = document.querySelector("#page");
-  pageDiv.innerHTML = form;
-  console.log("set page done");
+  pageDiv.innerHTML = formHTML;
+}
+
+function setLinks(){
+  const newSalon = document.querySelector("#form").getElementsByTagName("button");
+  newSalon[0].addEventListener("click", () => {
+    Redirect("/");
+  });
 }
 
 export default SalonModo;
