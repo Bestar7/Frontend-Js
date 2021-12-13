3917cbf2617937b697a7230edc14f3c01e955688
Joachim IPL <joachim.bastin@student.vinci.be>
2021-12-13 14:19:01 +0100

    update homePage

--- src/components/Pages/homePage.js
+++ src/components/Pages/homePage.js
@@ -7,40 +7,37 @@ import { Button as BootstrapButton} from "bootstrap";
 * 
 * Render the HomePage
 */
-let test=`
-<div class="container d-flex justify-content-center">
-  <div id="home" class="flex-center">
-    
-    <div id="connexion">
-      <form id="form">
-        <input type="email" placeholder="email" name="email" type="text"/>
-        <input type="password" placeholder="Mot de passe" name="password"/>
-        <input type="submit" class="btn btn-primary" value="Se connecter"></input>    <!--TODO-->
-        <a class="btn btn-primary" data-uri="/register">Creer une compte</a>
-      </form>
-    </div>
-    <br>
+let connexionHTML = 
+`<div id="connexion">
+  <form id="form">
+    <input type="email" placeholder="email" name="email" type="text"/>
+    <input type="password" placeholder="Mot de passe" name="password"/>
+    <input type="submit" class="btn btn-primary" value="Se connecter"></input>    <!--TODO-->
+    <a class="btn btn-primary" data-uri="/register">Creer une compte</a>
+  </form>
+</div>
+<br>`;
 
-    <div id="joinSalon" class="flex-center">
-      <form>
-        <input type = "text" placeholder="code salon" name="codeSalon"/>
-        <input type="submit" class="btn btn-primary" value="Joindre salon"></input>    <!--TODO-->
-      </form>
-    </div>
-    <br>
+let joinSalonHTML = 
+`<div id="joinSalon" class="flex-center">
+  <form>
+    <input type = "text" placeholder="code salon" name="codeSalon"/>
+    <input type="submit" class="btn btn-primary" value="Joindre salon"></input>    <!--TODO-->
+  </form>
+</div>
+<br>`;
 
-    <div id="newSalon" class="flex-center">
-      <button type="button" class="btn btn-primary" href="#" data-uri="/salonModo">Creer un salon</button>
-    </div>
-    <br>
+let newSalonHTML = 
+`<div id="newSalon" class="flex-center">
+  <button type="button" class="btn btn-primary" href="#" data-uri="/salonModo">Creer un salon</button>
+</div>
+<br>`;
 
-    <div id="jouer" class="flex-center">
-      <button type="button" class="btn btn-primary">Prêt</button>    <!--TODO-->
-    </div>
-    <br>
-
-  </div>
-</div>`;
+let jouerHTML = 
+`<div id="jouer" class="flex-center">
+  <button type="button" class="btn btn-primary">Prêt</button>    <!--TODO-->
+</div>
+<br>`;
 
 function HomePage() {
   setBasicPage();
@@ -51,7 +48,7 @@ function setBasicPage() {
   document.title = 'Ze Question';
   // SET BASIC PAGE
   const pageDiv = document.querySelector("#page");
-  pageDiv.innerHTML = test;
+  pageDiv.innerHTML = connexionHTML + joinSalonHTML + newSalonHTML + jouerHTML;
   console.log("set page done");
 }
 
