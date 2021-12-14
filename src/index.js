0ee03f34e07f997e7bdeaa7cd3532e490ebac4d3
Bestar7 <71338224+Bestar7@users.noreply.github.com>
2021-12-14 09:35:19 +0100

    fix Heroku deployement bug

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./stylesheets/main.css";
 
-import Navbar from "./components/NavBar/NavBar";
-import {Router} from "./Components/Router/Router";
+import Navbar from "./Components/NavBar/NavBar.js";
+import {Router} from "./Components/Router/Router.js";
 
 
 
