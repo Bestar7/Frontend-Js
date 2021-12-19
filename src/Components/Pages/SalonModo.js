ec26fbbe835c23ca8d6e4ef393f52588bab51c29
Joachim Bastin Local <joachim.bastin@gmail.com>
2021-12-19 06:59:23 +0100

    add can receive theme as checkbox

--- src/Components/Pages/SalonModo.js
+++ src/Components/Pages/SalonModo.js
@@ -1,7 +1,14 @@
 import { Redirect } from "../Router/Router.js";
 
 let idSalon;
-
+let checkboxes = "";
+const themesDebutHTML = 
+`<div id="themes">
+<form>
+  <legend>Quels thématiques voulez-vous ?</legend>`;
+const themesFinHTML = 
+`</form>
+</div>`;
 const formHTML = 
 `<div id="form">
   <form>
@@ -14,6 +21,7 @@ const formHTML =
 
 function SalonModo() {
   creerCodeSalon();
+  creerTheme();
   setBasicPage();
   setLinks();
 }
@@ -33,11 +41,35 @@ function creerCodeSalon(){
   });
 }
 
+function creerTheme(){
+  fetch("/api/questions/", {
+    method: "POST",
+    body: null,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+  .then((response) => {
+    if (!response.ok)
+      throw new Error("Error code : " + response.status + " : " + response.statusText);
+    let themes = response.json().themes;
+    for (i=0; i<themes.length ; i++){
+      checkboxes += 
+      `<input class="form-check-input" type="checkbox" id="theme`+i+`">
+      <label class="form-check-label" for="theme`+i+`">`+themes[i]+`</label>`
+    }
+    
+    return response.json();
+  });
+
+  
+}
+
 function setBasicPage() {
   document.title = 'Salon Administrateur';
   // SET BASIC PAGE
   const pageDiv = document.querySelector("#page");
-  pageDiv.innerHTML = formHTML;
+  pageDiv.innerHTML = formHTML+themesDebutHTML+checkboxes+themesFinHTML;
 }
 
 function setLinks(){
